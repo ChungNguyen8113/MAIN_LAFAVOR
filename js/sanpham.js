525db99091115872791a98007b3eef855bffa16b
Lanh189 <lananhho754@gmail.com>
2023-12-30 20:56:41 +0700

    lanh

--- js/sanpham.js
+++ js/sanpham.js
@@ -1,20 +1,62 @@
+
+
 document.addEventListener("DOMContentLoaded", function () {
-    // Đường dẫn tới tệp JSON chứa thông tin sản phẩm
     const jsonFilePath = "/json/ProductCategory.json";
-    
-    // Lấy thông tin sản phẩm từ tệp JSON
+
+    let allProducts; // Danh sách sản phẩm gốc
+    let displayedProducts; // Danh sách sản phẩm hiển thị sau khi tìm kiếm
+
     fetch(jsonFilePath)
         .then(response => response.json())
         .then(data => {
+            allProducts = [].concat(
+                data.banhmi,
+                data.banhquy,
+                data.banhtheomua,
+                data.banhkhac
+            );
+
+            // Ban đầu, danh sách hiển thị giống như danh sách gốc
+            displayedProducts = [...allProducts];
+
             const productContainer = document.getElementById('productContainer');
+            const searchInput = document.getElementById('searchInput');
+            const sortOption = document.getElementById('sortOption');
 
-            // Lặp qua các sản phẩm và tạo HTML động
-            data.banhmi.forEach(product => {
+            searchInput.addEventListener('input', function () {
+                const searchTerm = this.value.toLowerCase();
+
+                // Lọc sản phẩm theo điều kiện tìm kiếm
+                displayedProducts = allProducts.filter(product =>
+                    product.name.toLowerCase().includes(searchTerm)
+                );
+
+                // Sắp xếp và hiển thị sản phẩm sau khi tìm kiếm
+                sortAndDisplay();
+            });
+
+            sortOption.addEventListener('change', function () {
+                // Sắp xếp và hiển thị sản phẩm sau khi tìm kiếm
+                sortAndDisplay();
+            });
+
+            window.sortProducts = sortAndDisplay;
+
+            function displayProducts(productList) {
+                productContainer.innerHTML = '';
+
+                productList.forEach(product => {
+                    const productItem = createProductElement(product);
+                    productContainer.appendChild(productItem);
+                });
+            }
+
+            function createProductElement(product) {
                 const productItem = document.createElement('div');
                 productItem.className = 'col-lg-3 col-md-6 col-sm-6';
                 productItem.innerHTML = `
                     <div class="product__item">
-                        <div class="product__item__pic set-bg" style="background: url(${product.image})"> <br/>
+                        <div class="product__item__pic set-bg" style="background: url(${product.image})">
                             <div class="product__label">
                                 <span>${product.label}</span>
                             </div>
@@ -22,53 +64,38 @@ document.addEventListener("DOMContentLoaded", function () {
                         <br/>
                         <div class="product__item__text">
                             <h6><a href="#">${product.name}</a></h6>
-                            <div class="product__item__price">${product.price}</div>
+                            <div class="product__item__price">${product.price} VND</div>
                             <div class="cart_add">
                                 <a href="#">Thêm vào giỏ hàng</a>
                             </div>
                         </div>
                     </div>
                 `;
+                return productItem;
+            }
 
-                // Thêm sản phẩm vào container
-                productContainer.appendChild(productItem);
-            });
-
-            // Bạn có thể thêm logic phân trang ở đây nếu cần
-
-            // Lặp qua các sản phẩm và tạo HTML động
-            data.banhquy.forEach(product => {
-                const productItem = document.createElement('div');
-                productItem.className = 'col-lg-3 col-md-6 col-sm-6';
-                productItem.innerHTML = `
-                    <div class="product__item">
-                        <div class="product__item__pic set-bg" style="background: url(${product.image})"> <br/>
-                            <div class="product__label">
-                                <span>${product.label}</span>
-                            </div>
-                        </div>
-                        <br/>
-                        <div class="product__item__text">
-                            <h6><a href="#">${product.name}</a></h6>
-                            <div class="product__item__price">${product.price}</div>
-                            <div class="cart_add">
-                                <a href="#">Thêm vào giỏ hàng</a>
-                            </div>
-                        </div>
-                    </div>
-                `;
-
-                // Thêm sản phẩm vào container
-                productContainer.appendChild(productItem);
-            });
+            function sortAndDisplay() {
+                const selectedOption = sortOption.value;
+                console.log('Selected Option:', selectedOption);
+            
+                // Sắp xếp danh sách sản phẩm hiển thị
+                displayedProducts.sort((a, b) => {
+                    if (selectedOption === 'price') {
+                        console.log('Sorting by Price');
+                        console.log('a.price:', a.price);
+                        console.log('b.price:', b.price);
+                        return parseFloat(a.price) - parseFloat(b.price);
+                    } else if (selectedOption === 'name') {
+                        console.log('Sorting by Name');
+                        return a.name.localeCompare(b.name);
+                    }
+                });
+            
+                // Hiển thị danh sách sản phẩm đã sắp xếp
+                displayProducts(displayedProducts);
+            }
+            // Hiển thị toàn bộ danh sách sản phẩm khi tải trang
+            displayProducts(allProducts);
         })
         .catch(error => console.error('Lỗi khi lấy thông tin sản phẩm:', error));
 });
-
-
-
-
-
-
-
-
